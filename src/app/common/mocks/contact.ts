cd98998b3af2a8383bb08f614b2a17afa9c2450c
SergiyGenza <63913766+SergiyGenza@users.noreply.github.com>
2023-07-26 22:32:21 +0300

    finish contact page template

--- src/app/common/mocks/contact.ts
+++ src/app/common/mocks/contact.ts
@@ -3,25 +3,30 @@ export const CONTACT = {
   text: 'For More Information About Our Product & Services. Please Feel Free To Drop Us An Email. Our Staff Always Be There To Help You Out. Do Not Hesitate!',
   info: [
     {
-      mapIcon: '../../../assets/images/icons/map-icon.svg',
+      icon: '../../../assets/images/icons/map-icon.svg',
       title: 'Address',
-      text: '236 5th SE Avenue, New York NY10000, United States' 
+      text: '236 5th SE Avenue, New York NY10000, United States'
     },
     {
-      mapIcon: '../../../assets/images/icons/phone-icon.svg',
+      icon: '../../../assets/images/icons/phone-icon.svg',
       title: 'Phone',
-      text: 'Mobile: +(84) 546-6789 Hotline: +(84) 456-6789' 
+      text: 'Mobile: +(84) 546-6789 Hotline: +(84) 456-6789'
     },
     {
-      mapIcon: '../../../assets/images/icons/clock-icon.svg',
+      icon: '../../../assets/images/icons/clock-icon.svg',
       title: 'Working Time',
-      text: 'Monday-Friday: 9:00 - 22:00 Saturday- Sunday: 9:00 - 21:00' 
+      text: 'Monday-Friday: 9:00 - 22:00 Saturday- Sunday: 9:00 - 21:00'
     },
   ],
-  formLabels: {
+  form: {
     name: 'Your name',
     email: 'Email address',
     about: 'Subject',
     message: 'Message',
+    namePlaseholder: 'John',
+    emailPlaseholder: 'John@gmail.com',
+    aboutPlaseholder: 'This is an optional',
+    messagePlaseholder: 'Hi! i’d like to ask about',
+    btnTitle: 'Submit',
   }
 }
--- src/app/modules/contact/contact-page/contact-page.component.ts
+++ src/app/modules/contact/contact-page/contact-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CONTACT } from 'src/app/common/mocks/contact';
+import { SHOP } from 'src/app/common/mocks/shop';
 
 @Component({
   selector: 'app-contact-page',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-page.component.scss']
 })
 export class ContactPageComponent {
-
+  contact = CONTACT;
+  shop = SHOP;
 }
