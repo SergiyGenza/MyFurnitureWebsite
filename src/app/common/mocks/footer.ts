7faf2f5c871433d3ef3104bd0bacc11cc0e49b28
SergiyGenza <63913766+SergiyGenza@users.noreply.github.com>
2023-07-31 22:11:47 +0300

    fix: add missing footer mock

---
+++ src/app/common/mocks/footer.ts
@@ -0,0 +1,7 @@
+export const FOOTER = {
+  email: 'FuniroSome@mail.com',
+  btnTitle: 'SUBSCRIBE',
+  adress: '400 University Drive Suite 200 Coral Gables, FL 33134 USA',
+  copyright: '2023 furino. All rights reverved',
+
+}
