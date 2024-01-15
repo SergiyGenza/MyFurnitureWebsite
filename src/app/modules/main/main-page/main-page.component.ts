42ae6a658edf3c9ed298c9fcdbb2bde934467601
SergiyGenza <63913766+SergiyGenza@users.noreply.github.com>
2024-01-15 17:09:48 +0200

    Fix: add new fonts for banner component, remove comented code in main-page component

--- src/app/modules/main/main-page/main-page.component.ts
+++ src/app/modules/main/main-page/main-page.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { PRODUCTS } from 'src/app/common/mocks/products';
-import { Product, Sofa } from 'src/app/common/models/product.model';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Sofa } from 'src/app/common/models/product.model';
 import { ProductService } from 'src/app/common/services/product.service';
 import { SofaService } from 'src/app/common/services/sofa/sofa.service';
 
@@ -10,12 +9,10 @@ import { SofaService } from 'src/app/common/services/sofa/sofa.service';
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent implements OnInit {
-  // test
-  productsArray: Array<Product> = PRODUCTS;
-  /////////////////////
+export class MainPageComponent implements OnInit, OnDestroy {
   products: Observable<Sofa[]> | undefined;
   showSpinner: boolean = true;
+  subscription!: Subscription;
 
   constructor(
     private productService: ProductService,
@@ -26,17 +23,12 @@ export class MainPageComponent implements OnInit {
     this.getAllProducts();
   }
 
-  // test
-  public addProducts() {
-    this.productsArray.map((p) => {
-      this.productService.addProduct(p);
-    });
-    console.log('products added');
-  }
-  /////////////////////
-
   private getAllProducts() {
     this.products = this.sofaService.getAllProducts();
-    this.products.subscribe(() => this.showSpinner = false);
+    this.subscription = this.products.subscribe(() => this.showSpinner = false);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
