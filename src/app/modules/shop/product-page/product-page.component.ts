05724df35d18465c826a10af5424dd7d708f6410
SergiyGenza <63913766+SergiyGenza@users.noreply.github.com>
2025-04-24 21:57:59 +0300

    ref: shop module

--- src/app/modules/shop/product-page/product-page.component.ts
+++ src/app/modules/shop/product-page/product-page.component.ts
@@ -1,65 +1,57 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Product } from 'src/app/common/models/product.model';
 import { ProductService } from 'src/app/common/services/product.service';
 import { CartService } from 'src/app/services/cart.service';
 import { ComparisonService } from 'src/app/services/comparison.service';
 import { BreadcrumbComponent } from '../../../shared/breadcrumb/breadcrumb.component';
-import { NgIf, NgFor, NgClass, AsyncPipe, CurrencyPipe } from '@angular/common';
+import { NgClass, AsyncPipe, CurrencyPipe } from '@angular/common';
 import { SpinnerComponent } from '../../../shared/spinner/spinner.component';
 
 @Component({
-    selector: 'app-product-page',
-    templateUrl: './product-page.component.html',
-    styleUrls: ['./product-page.component.scss'],
-    standalone: true,
-    imports: [BreadcrumbComponent, NgIf, SpinnerComponent, NgFor, NgClass, AsyncPipe, CurrencyPipe]
+  selector: 'app-product-page',
+  templateUrl: './product-page.component.html',
+  styleUrls: ['./product-page.component.scss'],
+  standalone: true,
+  imports: [BreadcrumbComponent, SpinnerComponent, NgClass, AsyncPipe, CurrencyPipe]
 })
-export class ProductPageComponent implements OnInit, OnDestroy {
-  key: string;
-  product$: Observable<Product | undefined> | undefined;
-  quantity: number = 1;
-  showSpinner: boolean = true;
-  subscription!: Subscription;
-  tab = 1;
+export class ProductPageComponent implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  private readonly productService = inject(ProductService);
+  private readonly cartService = inject(CartService);
+  private readonly comparisonService = inject(ComparisonService);
 
-  constructor(
-    private route: ActivatedRoute,
-    private productService: ProductService,
-    private cartService: CartService,
-    private comparisonService: ComparisonService,
-  ) {
+  public product$!: Observable<Product | undefined>;
+  public quantity: number = 1;
+  public tab = 1;
+  private readonly key: string;
+
+  constructor() {
     this.key = this.route.snapshot.paramMap.get('key') as string;
   }
 
   ngOnInit(): void {
-    this.getProduct();
-  }
-
-  getProduct() {
     this.product$ = this.productService.getProductByName(this.key);
-    this.subscription = this.product$.subscribe(() => this.showSpinner = false);
   }
 
-  public increase() {
-    if (this.quantity == 99) return
-    return this.quantity = this.quantity + 1;
+  public increase(): number {
+    return this.quantity === 99
+      ? this.quantity
+      : this.quantity = this.quantity + 1;
   }
 
-  public decrease() {
-    return this.quantity < 2 ? this.quantity = this.quantity : this.quantity = this.quantity - 1;
+  public decrease(): number {
+    return this.quantity < 2
+      ? this.quantity = this.quantity
+      : this.quantity = this.quantity - 1;
   }
 
-  public addToCart(product: Product) {
+  public addToCart(product: Product): void {
     this.cartService.updateCart(product, this.quantity);
   }
 
-  public onProductCompare(product: Product) {
+  public onProductCompare(product: Product): void {
     this.comparisonService.addToCompareList(product);
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
   }
 }
--- src/app/modules/shop/shop-page/shop-page.component.ts
+++ src/app/modules/shop/shop-page/shop-page.component.ts
@@ -1,42 +1,31 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SHOP } from 'src/app/common/mocks/shop';
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Sofa } from 'src/app/common/models/product.model';
 import { SofaService } from 'src/app/common/services/sofa/sofa.service';
 import { BreadcrumbComponent } from '../../../shared/breadcrumb/breadcrumb.component';
 import { ShopFilterComponent } from '../shop-filter/shop-filter.component';
-import { NgIf, NgFor, AsyncPipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { SpinnerComponent } from '../../../shared/spinner/spinner.component';
 import { ProductCardComponent } from '../../../shared/product-card/product-card.component';
 import { BenefitsComponent } from '../../../shared/benefits/benefits.component';
 
 @Component({
-    selector: 'app-shop-page',
-    templateUrl: './shop-page.component.html',
-    styleUrls: ['./shop-page.component.scss'],
-    standalone: true,
-    imports: [BreadcrumbComponent, ShopFilterComponent, NgIf, SpinnerComponent, NgFor, ProductCardComponent, BenefitsComponent, AsyncPipe]
+  selector: 'app-shop-page',
+  templateUrl: './shop-page.component.html',
+  styleUrls: ['./shop-page.component.scss'],
+  standalone: true,
+  imports: [BreadcrumbComponent, ShopFilterComponent, SpinnerComponent, ProductCardComponent, BenefitsComponent, AsyncPipe]
 })
-export class ShopPageComponent implements OnInit, OnDestroy {
-  shop = SHOP;
-  products: Observable<Sofa[]> | undefined;
-  showSpinner: boolean = true;
-  subscription!: Subscription;
+export class ShopPageComponent implements OnInit {
+  public readonly sofaService = inject(SofaService);
 
-  constructor(
-    private sofaService: SofaService
-  ) { }
+  public readonly shop = SHOP;
+  public products: Observable<Sofa[]> | undefined;
+
+  constructor() { }
 
   ngOnInit(): void {
-    this.getAllProducts();
-  }
-
-  private getAllProducts() {
     this.products = this.sofaService.getAllProducts();
-    this.subscription = this.products.subscribe(() => this.showSpinner = false);
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
   }
 }
