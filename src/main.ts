52b1eec0871b76f3a3269faed1a11ab81fa5eb43
SergiyGenza <63913766+SergiyGenza@users.noreply.github.com>
2025-04-24 16:14:03 +0300

    ref: create new router file, remove old modules

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { AppRoutingModule } from './app/app-routing.module';
+import { appRoutes } from './app/app-routing';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environments';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule, AngularFireAuthModule, AngularFireModule.initializeApp(environment.firebaseConfig))]
+  providers: [
+    importProvidersFrom(BrowserModule, AngularFireAuthModule, AngularFireModule.initializeApp(environment.firebaseConfig)),
+    provideRouter(appRoutes)
+  ]
 })
   .catch(err => console.error(err));
